3084b01d2e8cfc3423ceb3619e9a47af45e68f7f
Rajan Middha-ThinkPad <middha.rajan@gmail.com>
2016-10-17 13:23:36 +0530

    check load on server

---
+++ scripts/shell/check_load.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+###___AUTHOR:RAJAN MIDDHA___###
+
+set -e
+
+
+get_root_size()
+{
+	ROOT_SIZE=`df -h | grep /dev/sda | awk '{print $5}' | tr -d '%'`
+	ROOT_THRESHOLD=75
+
+	if [ $ROOT_SIZE -gt $ROOT_THRESHOLD ];then
+		echo `date` 'Sending critical email. Disk almost full.'
+		mail -s 'Disk Space Alert' rajan.middha@innovaccer.com << EOF
+		Your root partition remaining free space is critically low. Used: $ROOT_SIZE%
+EOF
+	fi
+}
+
+get_free_mem()
+{
+	FREE_RAM_SIZE=`free -m | grep  Mem | awk '{print $4}'`
+	RAM_THRESHOLD=300
+	if [ $FREE_RAM_SIZE -lt $RAM_THRESHOLD ];then
+		echo `date` 'Sending critical email. Disk almost full.'
+		mail -s 'Disk Space Alert' rajan.middha@innovaccer.com << EOF
+		Your root partition remaining free space is critically low. Used: $FREE_RAM_SIZE"MB"
+EOF
+	fi
+
+}
+
+get_root_size
+get_free_mem
