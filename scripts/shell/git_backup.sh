fda60cad722481130dcc1bb0506e9770455af0ba
Rajan Middha <rajan.middha@innovaccer.com>
2016-10-25 13:30:26 +0530

    change old bckps from 7 to 15 days

--- scripts/shell/git_backup.sh
+++ scripts/shell/git_backup.sh
@@ -13,7 +13,7 @@ export AWS_DEFAULT_OUTPUT="json"
 ###############################################
 
 TIME=`date +%d-%b-%y`
-OLD_TIME=`date +%d-%b-%y --date="7 days ago"`
+OLD_TIME=`date +%d-%b-%y --date="15 days ago"`
 FILENAME="backup-$TIME.tar.gz"
 OLDFILENAME="backup-$OLD_TIME.tar.gz"
 SRC_DIR="src"
@@ -21,15 +21,19 @@ DEST_DIR="dest"
 BUCKET_NAME="innovaccer-git-backup"
 
 backup(){
+
+###### Create Backup ######
 mkdir -p old_bkp
 echo "Taking backup on $TIME"
 echo "tar -cpzf $DEST_DIR/$FILENAME $SRC_DIR"
 echo "Backup Created"
 
+###### Transfer Backup to S3 ######
 echo "Moving $FILENAME to s3"
 aws s3 mv $DEST_DIR/$FILENAME s3://$BUCKET_NAME/$FILENAME
 echo "Backup Transferred to s3" 
 
+###### Remove Old Backups ######
 echo "Removing old backups"
 aws s3 mv s3://$BUCKET_NAME/$OLDFILENAME old_bkp/
 rm -rf old_bkp
