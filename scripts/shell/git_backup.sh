0f7bf67ab17ed462c2280301e3b3626d9e72c7cd
Rajan Middha <rajan.middha@innovaccer.com>
2016-10-27 12:06:37 +0530

    finalize git_backup script

--- scripts/shell/git_backup.sh
+++ scripts/shell/git_backup.sh
@@ -17,36 +17,40 @@ export AWS_DEFAULT_OUTPUT="json"
 
 TIME=`date +%d-%b-%y`
 OLD_TIME=`date +%d-%b-%y --date="15 days ago"`
-FILENAME="backup-$TIME.tar.gz"
-OLDFILENAME="backup-$OLD_TIME.tar.gz"
-SRC_DIR="/mnt/gitlab-repositories"
-DEST_DIR="/mnt/gitlab-bkp-rj"
+FILENAME="git-backup-$TIME.tar.gz"
+OLDFILENAME="git-backup-$OLD_TIME.tar.gz"
+SRC_DIR="/mnt/gitlab-repositories/repositories"
+DEST_DIR="/mnt/gitlab-repositories/git-bkp"
 BUCKET_NAME="inno-git-backup"
 
 backup(){
 
 ###### Create Backup ######
 mkdir -p old_bkp $DEST_DIR
-echo "Taking backup on $TIME" >> $DEST_DIR/git_backup.log
+echo "Backup Initiated on $TIME" >> git_backup.log
 tar -cpzf $DEST_DIR/$FILENAME $SRC_DIR
 echo "Backup Created" >> git_backup.log
+echo "Backup FileName: $FILENAME" >> git_backup.log
 
-###### Transfer Backup to S3 ######
-echo "Moving $FILENAME to s3" >> $DEST_DIR/git_backup.log
-#aws s3 mv $DEST_DIR/$FILENAME s3://$BUCKET_NAME/$FILENAME
-aws s3 cp $DEST_DIR/$FILENAME s3://$BUCKET_NAME/$FILENAME
-echo "Backup Transferred to s3" >> $DEST_DIR/git_backup.log
+###### Transfer Backup to S3 #####
+echo "Moving backup file ["$FILENAME"] to s3 [ bucket name: "$BUCKET_NAME" ]" >> git_backup.log
+aws s3 mv $DEST_DIR/$FILENAME s3://$BUCKET_NAME/$FILENAME
+#aws s3 cp $DEST_DIR/$FILENAME s3://$BUCKET_NAME/$FILENAME
+echo "Backup Transferred to s3-bucket" >> git_backup.log
 
 ###### Remove Old Backups ######
-echo "Removing old backups" >> $DEST_DIR/git_backup.log
+echo "Looking for old backups backups to remove..." >> git_backup.log
+echo "old backup file: $OLDFILENAME"
 aws s3 mv s3://$BUCKET_NAME/$OLDFILENAME old_bkp/
 rm -rf old_bkp
-echo "Old backups removed" >> $DEST_DIR/git_backup.log
+echo "Old backup[filename: $OLDFILENAME ] removed" >> git_backup.log
 
 ###### Send notification mail for process update ######
 echo "Backup Process Completed Successfully...!!!" | mail -s "Backup Process" rajan.middha@innovaccer.com
 
-echo "Done..!!!" >> $DEST_DIR/git_backup.log
+echo "Done..!!!" >> git_backup.log
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* " >> git_backup.log
+echo " " >> git_backup.log
 }
 
 if [ ! -f backup.lock ];then
@@ -56,4 +60,3 @@ if [ ! -f backup.lock ];then
 else
 	echo "backup.lock exists. Plz Check"
 fi
-
