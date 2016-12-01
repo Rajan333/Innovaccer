cfffe24e35fb6bc0bd09d4e8bc6f0c65ac01b2c6
Rajan Middha <rajan.middha@innovaccer.com>
2016-12-01 15:34:07 +0530

    added mongo,redis,elastic,azkaban in ambari

---
+++ datashop_deploy_scripts/install_nginx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###__AUTHOR: RAJAN MIDDHA__###
+
+install_nginx(){
+	sudo mkdir -p /opt/packages
+	sudo chown -R $USER /opt
+	cd /opt/packages
+	wget http://nginx.org/packages/centos/7/x86_64/RPMS/nginx-1.10.0-1.el7.ngx.x86_64.rpm
+	sudo rpm -ivh nginx-1.10.0-1.el7.ngx.x86_64.rpm
+	sudo service nginx start
+ }
