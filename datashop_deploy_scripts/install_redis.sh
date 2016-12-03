504fa3fcd02315367adffe8c4a9cb8cfdac60061
Rajan Middha <rajan.middha@innovaccer.com>
2016-12-03 20:18:04 +0530

    update redis script

--- datashop_deploy_scripts/install_redis.sh
+++ datashop_deploy_scripts/install_redis.sh
@@ -9,11 +9,11 @@ cd /opt/packages
 ## Download Package ##
 echo "Downloading Redis Package.."
 wget http://download.redis.io/releases/redis-3.2.5.tar.gz
-tar -xzvf redis-3.2.5.tar.gz
+sudo  tar xzf redis-3.2.5.tar.gz
 cd redis-3.2.5
 
 ## Install Redis ##
 echo "Installing Redis.."
-make
-src/redis-server &
+sudo make
+sudo src/redis-server &
 echo "Redis Successfully Installed."
