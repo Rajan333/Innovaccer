df13a4f747fe2a8ced4fa16b135dbe3191507cc1
Rajan Middha <rajan.middha@innovaccer.com>
2016-12-05 14:55:52 +0530

    update & finalize ambari_setup.sh

--- datashop_deploy_scripts/ambari_setup.sh
+++ datashop_deploy_scripts/ambari_setup.sh
@@ -17,6 +17,10 @@ GIT_USERNAME=$1
 GIT_PASSWORD=$2
 
 
+## Install necessary packages ##
+sudo yum install git-core vim wget -y
+
+## Configure sudo less ssh ##
 echo "Configuring ssh to localhost" >> ~/ambari_setup.log
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
@@ -25,12 +29,14 @@ sudo chown -R $USER /opt/
 mkdir -p /opt/packages
 echo "sudo less ssh done" >> ~/ambari_setup.log
 
+## Clone Datashop-infra Repository ##
 echo "Cloning DataShop Infra Git Repo" >> ~/ambari_setup.log
 echo "machine git.innovaccer.com login $GIT_USERNAME password $GIT_PASSWORD" >> ~/.netrc
 git clone http://git.innovaccer.com/akif/Datashop-Infra.git
 rm -rf ~/.netrc
 echo "DataShop Infra Cloned" >> ~/ambari_setup.log
 
+## Install Other Requirements ##
 echo "Installing python-devel" >> ~/ambari_setup.log
 sudo yum install python-devel -y
 echo "python-devel Installed" >> ~/ambari_setup.log
