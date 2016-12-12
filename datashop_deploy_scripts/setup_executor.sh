7ea7aa6d8865ed1b4daffff892e9035e11d5a87a
Rajan Middha <rajan.middha@innovaccer.com>
2016-12-12 17:49:19 +0530

    setup executor

---
+++ datashop_deploy_scripts/setup_executor.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cat << EOM
+		+-------------------------------------------+
+		| This Script will setup executor on server |
+		| and install all required dependencies	    |
+		+-------------------------------------------+
+EOM
+
+GIT_USERNAME="platform"
+GIT_PASSWORD="innovation123"
+
+## Set permissions ##
+sudo chown -R $USER /opt
+mkdir -p /opt/packages/Jobs
+mkdir -p /opt/packages/jars
+cd /opt/packages/Jobs
+
+## Install sbt ##
+curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+sudo yum install sbt -y
+sudo yum install git-core -y
+
+## Clone Repo ##
+echo "machine git.innovaccer.com login $GIT_USERNAME password $GIT_PASSWORD" >> ~/.netrc
+git init
+git remote add origin http://git.innovaccer.com/Datashop/Jobs.git
+git pull origin project/development
+
+## Setup Executor ##
+cd executor
+sbt clean compile assembly
+#cp -afv target/scala-2.11/com/datashop/executors-assembly-1.0.jar /opt/packages/jars/
+cd target/scala-*
+cp -afv com/datashop/executors-assembly-1.0.jar /opt/packages/jars/
+cd
+
+echo "Executor Setup Process Successfully Completed"
+
+
+
